48e38190293720ee47066a3764eaf883e83da6f4
Veronika Pomyateeva <pomyateevav@gmail.com>
2023-06-25 12:34:03 -0400

    fixed issues with process varible and google api key not working

--- src/App.js
+++ src/App.js
@@ -6,24 +6,51 @@ import {
   HStack,
   IconButton,
   Input,
+  Skeleton,
+  SkeletonText,
   Text,
 } from '@chakra-ui/react'
-import { FaLocationArrow, FaTimes } from 'react-icons/fa'
+import { FaLocationArrow, FaTimes } from 'react-icons/fa';
+
+// hook provides a variable to check if map is loaded
+import { useJsApiLoader, GoogleMap } from '@react-google-maps/api';
+
+// where to center the map, Empire State Building 
+const center = {
+  lat: 40.748817,
+  lng: -73.985428
+};
 
 function App() {
+
+  // loading google maps script
+  const { isLoaded } = useJsApiLoader({
+    googleMapsApiKey: process.env.REACT_APP_GOOGLE_MAPS_API_KEY,
+  });
+
+  // if not loaded display a chakra ui component demonstrating loading
+  if(!isLoaded) {
+    return <SkeletonText />
+  };
+
   return (
     <Flex
       position='relative'
       flexDirection='column'
       alignItems='center'
-      bgColor='blue.200'
-      bgImage='https://source.unsplash.com/random/2000x1100/?geography'
-      bgPos='bottom'
+      // bgImage='https://source.unsplash.com/random/2000x1100/?travel'
+      // bgPos='bottom'
       h='100vh'
       w='100vw'
     >
-      <Box position='absolute' left={0} top={0} h='100%' w='100%'></Box>
+      <Box position='absolute' left={0} top={0} h='100%' w='100%'>
+        {/* Display Google Maps Box */}
+          {/* Style of the map box is 100% of the parent */}
+          <GoogleMap center={center} zoom={15} mapContainerStyle={{ width:'100%', height:'100%'}}>
+          {/* Display markers and directions */}
 
+        </GoogleMap>
+      </Box>
       <Box
         p={4}
         borderRadius='lg'
